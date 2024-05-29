26a63fa3f148dfc941ee51f9c527acc48b4c1ccd
GlenWhiteII <glen2white@live.com>
2024-05-28 21:52:25 -0700

    feat: added login page

--- app.js
+++ app.js
@@ -10,17 +10,14 @@ const port = process.env.PORT || 3034;
 
 // Middleware
 app.set("view engine", "ejs");
-const userRouter = require("./routes/users");
 app.use("/", express.static("public"));
 
 // Config
 const mongoURI = "mongodb://localhost:27017/Entendre";
 
 // Routes
-app.get("/", (req, res) => {
-  res.render("signup");
-});
-app.use("/users", userRouter);
+const routes = require("./routes/users");
+app.use("/", routes);
 // Models
 
 // App Listen
