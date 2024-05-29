d5b7ff5ab07536458c814fe2b87088094bad61f0
GlenWhiteII <glen2white@live.com>
2024-05-28 20:01:53 -0700

    feat: created form for sign up and added basic styling

--- app.js
+++ app.js
@@ -10,8 +10,18 @@ const port = process.env.PORT || 3034;
 
 // Middleware
 app.set("view engine", "ejs");
+const userRouter = require("./routes/users");
+app.use("/", express.static("public"));
 
 // Config
 const mongoURI = "mongodb://localhost:27017/Entendre";
 
+// Routes
+app.get("/", (req, res) => {
+  res.render("signup");
+});
+app.use("/users", userRouter);
 // Models
+
+// App Listen
+app.listen(port);
